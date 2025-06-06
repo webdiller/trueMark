17243298c2b5d4c6d7308e506d1bfaa16c8abaf6
webdiller <eugenefromrus@gmail.com>
2025-06-07 06:46:46 +1000

    upd: some breaks and changes

--- src/views/ViewHome.tsx
+++ src/views/ViewHome.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import {Alert} from '@gravity-ui/uikit';
+import { Link } from '@tanstack/react-router';
 
 type Props = {}
 
 export const ViewHome = (props: Props) => {
   return (
-    <div>
-      <Alert theme="normal" title="Normal" message="Normal theme" />
+    <div className='container space-y-2 py-10 flex flex-col'>
+      <Link to='/sign-up'>Регистрация</Link>
+      <Link to='/sign-in'>Вход</Link>
+      <Link to='/surveys'>Опросы</Link>
     </div>
   )
 }
